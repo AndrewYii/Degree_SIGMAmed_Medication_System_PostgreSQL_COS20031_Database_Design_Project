62c4128d4720e149c1cda7df0aece029eeb2a93b
sinmim-0816 <janefam88@gmail.com>
2025-10-23 15:21:13 +0800

    v3.0

--- schema.sql
+++ schema.sql
@@ -33,9 +33,82 @@ COMMIT;
 BEGIN;
 
 -- User Table (base table for different roles)
-
+CREATE TABLE IF NOT EXISTS "User" (
+    "UserId" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "ClinicalInstitutionID" UUID,
+    "Username" VARCHAR(50) UNIQUE NOT NULL,
+    "Email" CITEXT UNIQUE NOT NULL,
+    "PasswordHash" VARCHAR(255) NOT NULL,
+    "Role" ENUM ('doctor', 'patient', 'admin') NOT NULL,
+    "ICPassportNumber" VARCHAR(50) UNIQUE NOT NULL,
+    "FirstName" VARCHAR(100) NOT NULL,
+    "LastName" VARCHAR(100) NOT NULL,
+    "Phone" VARCHAR(20) NOT NULL,
+    "DateOfBirth" DATE NOT NULL,
+    "FcmKey" VARCHAR(255)  NULL,
+    "ProfilePictureUrl" TEXT NULL,
+    "IsActive" BOOLEAN DEFAULT TRUE,
+    "IsDeleted" BOOLEAN DEFAULT FALSE
+);
 -- Add foreign key constraint to link User to ClinicalInstitution
 ALTER TABLE "User" ADD CONSTRAINT "fk_user_clinicalinstitution" FOREIGN KEY ("ClinicalInstitutionId") REFERENCES "ClinicalInstitution"("ClinicalInstituionID");
 
 COMMIT;
 -- End of the User Table creation
+
+-- Start transaction for Creating Medication Table
+BEGIN;
+
+-- Medication Table
+CREATE TABLE IF NOT EXISTS "Medication" (
+    "MedicationID" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "ClinicalInstitutionID" UUID
+    "MedicationName" VARCHAR(100) NOT NULL,
+    "TotalAmount" INT NOT NULL,
+    "IsDeleted" BOOLEAN DEFAULT FALSE
+);
+-- Add foreign key constraint to link Medication to ClinicalInstitution
+ALTER TABLE "Medication" ADD CONSTRAINT "fk_medication_clinicalinstitution" FOREIGN KEY ("ClinicalInstitutionId") REFERENCES "ClinicalInstitution"("ClinicalInstituionID");
+
+COMMIT;
+-- End of the Medication Table creation
+
+-- Start transaction for Creating Medication Table
+BEGIN;
+
+-- Medication Log Table
+CREATE TABLE IF NOT EXISTS "MedicationLog" (
+    "MedicationID" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "ClinicalInstitutionID" UUID
+    "MedicationName" VARCHAR(100) NOT NULL,
+    "TotalAmount" INT NOT NULL,
+    "IsDeleted" BOOLEAN DEFAULT FALSE
+);
+-- Add foreign key constraint to link Medication to ClinicalInstitution
+ALTER TABLE "Medication" ADD CONSTRAINT "fk_medication_clinicalinstitution" FOREIGN KEY ("ClinicalInstitutionId") REFERENCES "ClinicalInstitution"("ClinicalInstituionID");
+
+COMMIT;
+-- End of the Medication Table creation
+
+
+
+
+
+
+
+-- Start transaction for Creating Medication Table
+BEGIN;
+
+-- Medication Log Table
+CREATE TABLE IF NOT EXISTS "MedicationLog" (
+    "MedicationID" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "ClinicalInstitutionID" UUID
+    "MedicationName" VARCHAR(100) NOT NULL,
+    "TotalAmount" INT NOT NULL,
+    "IsDeleted" BOOLEAN DEFAULT FALSE
+);
+-- Add foreign key constraint to link Medication to ClinicalInstitution
+ALTER TABLE "Medication" ADD CONSTRAINT "fk_medication_clinicalinstitution" FOREIGN KEY ("ClinicalInstitutionId") REFERENCES "ClinicalInstitution"("ClinicalInstituionID");
+
+COMMIT;
+-- End of the Medication Table creation
