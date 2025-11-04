24fcdcb22b5c57e116f6478047abc200449e6a24
Andrew Teck Foon Yii <andrewyii0801@gmail.com>
2025-11-04 21:55:49 +0800

    v11.0
    - Create Patient Symptom Table

---
+++ create_patient_symptom.sql
@@ -0,0 +1,20 @@
+-- Set search path for current session
+SET search_path TO "SIGMAmed", public;
+-- Ensure uuid-ossp extension is available
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+-- Start transaction for Creating Patient Symptom Table
+BEGIN;
+
+CREATE TABLE "SIGMAmed"."PatientSymptom" (
+    -- for supabase, need to use extensions.uuid_generate_v4()
+    "PatientSymptomId" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "MedicalHistoryId" UUID NOT NULL REFERENCES "SIGMAmed"."MedicalHistory"("MedicalHistoryId") ON DELETE CASCADE,
+    "SymptomName" VARCHAR(100) NOT NULL,
+    UNIQUE("MedicalHistoryId", "SymptomName")
+);
+
+COMMENT ON TABLE "SIGMAmed"."PatientSymptom" IS 'Symptoms associated with medical history entries';
+
+-- Commit transaction for Creating Patient Symptom Table
+COMMIT;
