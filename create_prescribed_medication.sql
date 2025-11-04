78f7e59d9550717fd1aff02ec1ff1c25cd8b6ed2
Andrew Teck Foon Yii <andrewyii0801@gmail.com>
2025-11-04 22:31:02 +0800

    v16.0
    - Create Prescribed Medication Table

---
+++ create_prescribed_medication.sql
@@ -0,0 +1,23 @@
+-- Set search path for current session
+SET search_path TO "SIGMAmed", public;
+
+-- Start transaction for Creating Prescribed Medication Table
+BEGIN;
+
+CREATE TABLE "SIGMAmed"."PrescribedMedication" (
+    -- for supabase, need to use extensions.uuid_generate_v4()
+    "PrescribedMedicationId" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "PrescriptionId" UUID NOT NULL REFERENCES "SIGMAmed"."Prescription"("PrescriptionId") ON DELETE CASCADE,
+    "MedicationId" UUID NOT NULL REFERENCES "SIGMAmed"."Medication"("MedicationID") ON DELETE RESTRICT,
+    "StartDate" DATE NOT NULL,
+    "EndDate" DATE NOT NULL,
+    "DosageInstruction" TEXT,
+    "IsDeleted" BOOLEAN DEFAULT FALSE,
+    CONSTRAINT chk_prescribed_medication_dates CHECK ("StartDate" <= "EndDate"),
+    UNIQUE("PrescriptionId", "MedicationId")
+);
+
+COMMENT ON TABLE "SIGMAmed"."PrescribedMedication" IS 'Individual medications within a prescription with dosage details';
+
+-- Commit transaction for Creating Prescribed Medication Table
+COMMIT;
