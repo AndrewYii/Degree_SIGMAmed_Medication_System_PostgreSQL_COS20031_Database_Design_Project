9c4325e607afb71b8286d86940cff9c48653aeeb
Andrew Teck Foon Yii <andrewyii0801@gmail.com>
2025-11-04 21:21:07 +0800

    v4.0
    - Create doctor table

---
+++ create_doctor.sql
@@ -0,0 +1,22 @@
+-- Set search path for current session
+SET search_path TO "SIGMAmed", public;
+
+-- Start transaction for Creating Doctor Table
+BEGIN;
+
+CREATE TABLE "SIGMAmed"."Doctor" (
+    -- for supabase, need to use extensions.uuid_generate_v4()
+    "UserId" UUID PRIMARY KEY REFERENCES "SIGMAmed"."User"("UserId") ON DELETE CASCADE,
+    "MedicalLicenseNumber" VARCHAR(50) UNIQUE NOT NULL,
+    "Specialization" VARCHAR(100) NOT NULL,
+    "YearOfExperience" INT NOT NULL,
+    "MedicalSchool" VARCHAR(255) NOT NULL,
+    "Bio" TEXT,
+    CONSTRAINT chk_experience CHECK ("YearOfExperience" >= 0 AND "YearOfExperience" <= 60)
+);
+
+COMMENT ON TABLE "SIGMAmed"."Doctor" IS 'Doctor-specific information extending User table';
+COMMENT ON COLUMN "SIGMAmed"."Doctor"."MedicalLicenseNumber" IS 'Unique medical license number for the doctor';
+
+-- Commit transaction for Creating Doctor Table
+COMMIT;
