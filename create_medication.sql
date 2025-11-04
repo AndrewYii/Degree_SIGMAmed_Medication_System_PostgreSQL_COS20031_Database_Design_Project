44657f3cc697bb3145d895c84b030932add99ebf
Andrew Teck Foon Yii <andrewyii0801@gmail.com>
2025-11-04 21:58:25 +0800

    v12.0
    - Create Medication Table

---
+++ create_medication.sql
@@ -0,0 +1,23 @@
+-- Set search path for current session
+SET search_path TO "SIGMAmed", public;
+-- Ensure uuid-ossp extension is available
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+-- Start transaction for Creating Medication Table
+BEGIN;
+
+CREATE TABLE "SIGMAmed"."Medication" (
+    -- for supabase, need to use extensions.uuid_generate_v4()
+    "MedicationID" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "ClinicalInstitutionID" UUID NOT NULL REFERENCES "SIGMAmed"."ClinicalInstitution"("ClinicalInstitutionID") ON DELETE CASCADE,
+    "MedicationName" VARCHAR(100) NOT NULL,
+    "TotalAmount" INT NOT NULL,
+    "IsDeleted" BOOLEAN DEFAULT FALSE,
+    CONSTRAINT chk_total_amount CHECK ("TotalAmount" >= 0),
+    UNIQUE("ClinicalInstitutionID", "MedicationName")
+);
+
+COMMENT ON TABLE "SIGMAmed"."Medication" IS 'Medication inventory per clinical institution';
+
+-- Commit transaction for Creating Medication Table
+COMMIT;
