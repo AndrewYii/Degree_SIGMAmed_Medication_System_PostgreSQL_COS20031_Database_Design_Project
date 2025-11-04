7c9483c5a17fbe7748e25456fbc96d19fa03a83a
Andrew Teck Foon Yii <andrewyii0801@gmail.com>
2025-11-04 21:39:03 +0800

    v9.0
    - Create Medical History Table

---
+++ create_medical_history.sql
@@ -0,0 +1,22 @@
+-- Set search path for current session
+SET search_path TO "SIGMAmed", public;
+-- Ensure uuid-ossp extension is available
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+-- Start transaction for Creating Medical History Table
+BEGIN;
+
+CREATE TABLE "SIGMAmed"."MedicalHistory" (
+    -- for supabase, need to use extensions.uuid_generate_v4()
+    "MedicalHistoryId" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "PatientId" UUID NOT NULL REFERENCES "SIGMAmed"."Patient"("UserId") ON DELETE CASCADE,
+    "DiseaseName" VARCHAR(100) NOT NULL,
+    "Severity" INT DEFAULT 0,
+    "IsDeleted" BOOLEAN DEFAULT FALSE,
+    CONSTRAINT chk_severity CHECK ("Severity" >= 0 AND "Severity" <= 10)
+);
+
+COMMENT ON TABLE "SIGMAmed"."MedicalHistory" IS 'Patient medical history and chronic conditions';
+
+-- Commit transaction for Creating Medical History Table
+COMMIT;
