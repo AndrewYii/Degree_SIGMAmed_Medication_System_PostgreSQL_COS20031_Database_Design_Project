9961dc8f8db5e95d6292d06d1eb0c6c34068180b
Andrew Teck Foon Yii <andrewyii0801@gmail.com>
2025-11-04 22:58:27 +0800

    v23.0
    - Create Patient Report Table

---
+++ create_patient_report.sql
@@ -0,0 +1,21 @@
+-- Set search path for current session
+SET search_path TO "SIGMAmed", public;
+
+-- Start transaction for Creating Patient Report Table
+BEGIN;
+
+CREATE TABLE "SIGMAmed"."PatientReport" (
+    -- for supabase, need to use extensions.uuid_generate_v4()
+    "PatientReportID" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "DoctorId" UUID NOT NULL REFERENCES "SIGMAmed"."Doctor"("UserId") ON DELETE RESTRICT,
+    "PatientId" UUID NOT NULL REFERENCES "SIGMAmed"."Patient"("UserId") ON DELETE CASCADE,
+    "Status" "SIGMAmed".patient_report_status_enum NOT NULL,
+    "Reason" TEXT,
+    "AttachmentDirectory" TEXT,
+    "IsDeleted" BOOLEAN DEFAULT FALSE
+);
+
+COMMENT ON TABLE "SIGMAmed"."PatientReport" IS 'Patient health reports and communications';
+
+-- Commit transaction for Creating Patient Report Table
+COMMIT;
